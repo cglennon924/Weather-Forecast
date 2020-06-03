8c5e54bcc2d2a448e4c0e0b1fe0f8d5baa41f793
cglennon924 <63322716+cglennon924@users.noreply.github.com>
2020-06-03 10:24:56 -0400

    added images

--- Javascript/script.js
+++ Javascript/script.js
@@ -7,17 +7,18 @@ $(document).ready(function () {
     // Set todays date for Current Weather 
     var todaysDate = moment().format("LLLL");
     $("#todaysDate").text(todaysDate);
-
-
-
-
+    var citySearch = JSON.parse(localStorage.getItem("searchCities"))
     var queryURL = "https://api.openweathermap.org/data/2.5/forecast?id=" + location + "&appid=" + apiKey;
+
+
+   
 
     //Creating a button
     $('.btn').on("click", function (event) {
         event.preventDefault()
-
-        var citySearch = JSON.parse(localStorage.getItem("searchCities"))
+       location = $("#search").val().trim()
+        // var queryURL = "https://api.openweathermap.org/data/2.5/forecast?id=" + location + "&appid=" + apiKey;
+        
         var favCity = $('#Fav-City')
         $('#Fav-City').empty()
 
@@ -63,22 +64,22 @@ $(document).ready(function () {
                 console.log(lat)
                 console.log(lon)
                 uvIndex(lat, lon)
-            // Transfer content to HTML
-            $("#city").html("<h1>" + responseCW.name + " Weather Details</h1>");
-           
-            $("#uv").text("UV: " + responseCW.main.uv)
-           
-        // // Log the data in the console as well
-            console.log("Wind Speed: " + responseCW.wind.speed);
-            console.log("Humidity: " + responseCW.main.humidity);
-        // console.log("UV: " + response.main.uv)
-        })
+                // Transfer content to HTML
+                $("#city").html("<h1>" + responseCW.name + " Weather Details</h1>");
+
+                $("#uv").text("UV: " + responseCW.main.uv)
+
+                // // Log the data in the console as well
+                console.log("Wind Speed: " + responseCW.wind.speed);
+                console.log("Humidity: " + responseCW.main.humidity);
+                // console.log("UV: " + response.main.uv)
+            })
 
 
 
 
 
-            
+
         }
 
 
@@ -93,19 +94,19 @@ $(document).ready(function () {
         $.ajax({
             url: uvQueryURL,
             method: "GET"
-        }).then(function(responseHeat) {
+        }).then(function (responseHeat) {
             console.log(responseHeat);
             $("#uv").text("UV Index: ")
             uvValue = $("<span>").text(responseHeat.value)
             $("#uv").append(uvValue)
             if (responseHeat.value <= 3) {
-            uvValue.attr("style", "background-color: white;")
+                uvValue.attr("style", "background-color: white;")
             } else if (responseHeat.value > 3 && responseHeat.value <= 6) {
-            uvValue.attr("style", "background-color: yellow;")
-            }else if (responseHeat.value > 6 && responseHeat.value <= 9) {
-            uvValue.attr("style", "background-color: orange;")
-            } else{
-            uvValue.attr("style", "background-color: red;")
+                uvValue.attr("style", "background-color: yellow;")
+            } else if (responseHeat.value > 6 && responseHeat.value <= 9) {
+                uvValue.attr("style", "background-color: orange;")
+            } else {
+                uvValue.attr("style", "background-color: red;")
             };
         });
 
@@ -117,31 +118,47 @@ $(document).ready(function () {
     function fiveDay() {
         var forecast = $('#fiveDayForecast')
         var forecastQueryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + location + "&appid=" + apiKey;
+     
         $('#fiveDayForecast').empty()
-
+        console.log(forecastQueryURL)
         $.ajax({
             url: forecastQueryURL,
             method: "GET"
         }).then(function (response) {
+            console.log(response)
             for (var i = 5; i <= 40; i += 8) {
                 var futureDates = moment(response.list[i].dt_txt).format("MM/DD/YYYY");;
                 console.log(futureDates)
-                cardDiv = $("<div>");
-                cardDiv.addClass("col col-5-day mx-1")
-                cardBody = $("<div>");
-                cardBody.addClass("card-body forecast-card");
-                cardTitle = $("<h5>").text(futureDates)
-                cardBody.text(response.list[i].main.temp);
-                cardDiv.append(cardBody);
+                cardDiv = $('<div class = "card" style="width: 18rem;" >');
+                cardImage= $('<img class="card-img-top" src="http://openweathermap.org/img/wn/'+ response.list[i].weather[0].icon+'@2x.png" alt="Card image cap">');
+                cardBody = $('<div class="card-body">');
+                cardBody.addClass("forecast-card");
+                cardTitle = $('<h5 class="card-title">').text(futureDates);
+                cardP1= $('<p class="card-text">').text(response.list[i].main.temp);
+                cardP2= $('<p class="card-text">').text(response.list[i].main.temp);
+                cardP3= $('<p class="card-text">').text(response.list[i].main.temp);
                 cardBody.append(cardTitle);
-
+                cardBody.append(cardP1);
+                cardBody.append(cardP2);
+                cardBody.append(cardP3);
+                cardDiv.append(cardImage);
+                cardDiv.append(cardBody);
+                
+                //                 <div class="card" style="width: 18rem;">
+                //   <img class="card-img-top" src="..." alt="Card image cap">
+                //   <div class="card-body">
+                //     <h5 class="card-title">Card title</h5>
+                //     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                //     <a href="#" class="btn btn-primary">Go somewhere</a>
+                //   </div>
+                // </div>
                 // Log the queryURL
                 console.log(queryURL);
 
                 // Log the resulting object
                 console.log(response);
-                $(".card-body").append(cardDiv)
-            
+                $("#forecastCities").append(cardDiv)
+
 
 
                 // Log the data in the console as well
@@ -150,11 +167,11 @@ $(document).ready(function () {
                 //console.log("UV: " + response.main.uv)
 
                 // Transfer content to HTML
-            //     $("#city").html("<h1>" + response.name + " Weather Details</h1>");
-            //     $("#wind").text("Wind Speed: " + response.wind.speed);
-            //     $("#humidity").text("Humidity: " + response.main.humidity);
-            //     $("#uv").text("UV: " + response.main.uv)
-             }
+                //     $("#city").html("<h1>" + response.name + " Weather Details</h1>");
+                //     $("#wind").text("Wind Speed: " + response.wind.speed);
+                //     $("#humidity").text("Humidity: " + response.main.humidity);
+                //     $("#uv").text("UV: " + response.main.uv)
+            }
 
         })
 
